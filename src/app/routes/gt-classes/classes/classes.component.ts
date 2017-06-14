be312cb788549cd4a731de183ebe51ff26a1244c
Mr.jack <yoo@Stars-iMac.local>
2017-06-14 10:08:01 +0800

    update: more pages

--- src/app/routes/gt-classes/classes/classes.component.ts
+++ src/app/routes/gt-classes/classes/classes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-classes',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  chooseClass() {
+    this.router.navigate(['/classes/classes/students']);
+  }
 }

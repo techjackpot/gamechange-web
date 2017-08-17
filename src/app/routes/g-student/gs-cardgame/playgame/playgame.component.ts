fce47c898059d05b56cfaff6b9f48671625cf9be
Hiroshi <zsniper117@gmail.com>
2017-08-17 23:50:09 +0900

    update: leaderboard

--- src/app/routes/g-student/gs-cardgame/playgame/playgame.component.ts
+++ src/app/routes/g-student/gs-cardgame/playgame/playgame.component.ts
@@ -13,16 +13,42 @@ export class PlaygameComponent implements OnInit {
 	attendClasses = [];
 	selectedClass = null;
   currentStudent;
+  studentList = [];
 
   constructor(private router: Router, private dataService: DataService, private authService: AuthService) { }
+
+  
+  getIndexOfUsers(users,user_id) {
+    let index = -1;
+    users.forEach((user, i) => {
+      if(user._id == user_id) {
+        index = i;
+      }
+    });
+    return index;
+  }
 
   ngOnInit() {
     this.currentStudent = this.authService.getUser();
   	this.dataService.getAttendClasses({ student_id: this.dataService.getStudentID() }).subscribe( (response) => {
   		this.attendClasses = response.Classes;
   	});
+    this.dataService.getStudentList().subscribe( (students) => {
+      this.studentList = students;
+    });
   }
+
 	playGame(classInfo) {
 		this.router.navigate(['/cardgame/playgame/', classInfo._id]);
 	}
+  viewLeaderboard(classInfo) {
+    console.log(classInfo);
+    this.selectedClass = classInfo;
+    this.selectedClass.Players.sort(function(a, b) {
+      return parseFloat(b.Point) - parseFloat(a.Point);
+    });
+  }
+  cancelLeaderboard() {
+    this.selectedClass = null;
+  }
 }
